348f7046d2a82045743e7fea83516a5983b085ad
leftamarc <marcluisaquino@gmail.com>
2025-04-14 20:44:28 +0800

    Basic Flask Structure and adapted header tabs
    Implemented basic flask structure as per the week 7 lecture on Flask.
    
    Also implemented "header.html" which is shared among the new tab pages, i.e. "home.html", "get.html", etc. This adapts the first version of the header tabs which was created without being implemented in a server context.
    
    Clicking on a tab in the header now runs a server get request to fetch the route of the desired endpoint. Default is still the home page.

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/get')
+def get():
+    return render_template('get.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+@app.route('/share')
+def share():
+    return render_template('share.html')
