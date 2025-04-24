aacb0134b0fd6bdaad0c47d071e8ab9cb6efc1fd
UWA23475224 <23475224@student.uwa.edu.au>
2025-04-24 20:35:55 +0800

    implemented basic flask db structure, no DB yet

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
 import os
 
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+default_database_location = 'sqlite:///' + os.path.join(baseddir, 'app.db')
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_database_location
