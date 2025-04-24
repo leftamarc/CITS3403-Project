1b04483c5e5cef9a5332087aa0fe3cdcb919192f
UWA23475224 <23475224@student.uwa.edu.au>
2025-04-24 20:50:51 +0800

    fixed typo in config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-default_database_location = 'sqlite:///' + os.path.join(baseddir, 'app.db')
+default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_database_location
