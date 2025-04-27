72b40477d20052356c3278fdd6166f5c100ea53c
UWA23475224 <23475224@student.uwa.edu.au>
2025-04-27 23:29:20 +0800

    Restructured file layout for readability purposes

--- app/routes.py
+++ app/routes.py
@@ -4,24 +4,24 @@ from app import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('main/home.html')
 
 @app.route('/get')
 def get():
-    return render_template('get.html')
+    return render_template('main/get.html')
 
 @app.route('/view')
 def view():
-    return render_template('view.html')
+    return render_template('main/view.html')
 
 @app.route('/share')
 def share():
-    return render_template('share.html')
+    return render_template('main/share.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('main/login.html')
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('main/register.html')
