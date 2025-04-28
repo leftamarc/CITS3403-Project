29d6a503a746e7ca779d8f91033c0e7c499abd8b
UWA23475224 <23475224@student.uwa.edu.au>
2025-04-28 22:45:42 +0800

    app now checks that steam_api_key env var is it
    app will no longer launch if you haven't set your steam env var, see readme for instructions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+import os
+import sys
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
+
+#Checks for STEAM_API_KEY
+STEAM_API_KEY = os.getenv("STEAM_API_KEY")
+if not STEAM_API_KEY:
+    print("Error: STEAM_API_KEY environment variable is not set.")
+    sys.exit(1)
 
 app = Flask(__name__)
 app.config.from_object(Config)
