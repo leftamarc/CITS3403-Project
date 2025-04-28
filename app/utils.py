286a4a8273f71c198123bfbd397a1642ed847709
UWA23475224 <23475224@student.uwa.edu.au>
2025-04-29 04:25:58 +0800

    updated api calls
    GetPlayerSummaries now reports whether the inputed steam id belongs to a private account as an exception. All functions report difference between request error and no data error

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,16 @@ STEAM_API_KEY = os.getenv("STEAM_API_KEY")
 
 #                                                              //STEAM WEB API FUNCTIONS//
 
+
+class PrivateAccount(Exception):
+    pass
+
+class NoMatches(Exception):
+    pass
+
+class RequestFail(Exception):
+    pass
+
 #Fetches achievement data for a given game by app id, returns a list of tuples of the form (achievement_name, achievement_percentage)
 def GetGlobalAchievementPercentagesForApp(app_id):
     url = f"http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid={app_id}"
@@ -12,28 +22,28 @@ def GetGlobalAchievementPercentagesForApp(app_id):
         with urllib.request.urlopen(url) as response:
             data = response.read()
             if not data:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+                raise NoMatches("No matching data for input parameters")
             result = json.loads(data) 
             achievements = result["achievementpercentages"]["achievements"]
             return [(achievement["name"], achievement["percent"]) for achievement in achievements]
     except Exception as e:
-        #Request failed
-        raise Exception(f"Error: {e}")
+        raise RequestFail(f"Error: {e}")
 
-#Fetches profile data for a given player, returns steamid, username and avatar_url
+#Fetches profile data for a given player, returns steamid, username, avatar_url
 def GetPlayerSummaries(steam_id):
     url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={steam_id}"
     try:
         with urllib.request.urlopen(url) as response:
             data = response.read()
             if not data:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+                raise NoMatches("No matching data for input parameters")
             result = json.loads(data) 
             player_summary = result["response"]["players"][0]
+            if player_summary["communityvisibilitystate"] != 3:
+                raise PrivateAccount("The provided account is private")
             return player_summary["steamid"], player_summary["personaname"], player_summary["avatarfull"]
     except Exception as e:
-        #Request failed
-        raise Exception(f"Error: {e}")
+        raise RequestFail(f"Error: {e}")
 
 
 #Fetches achievement data for a given player, returns the games name and a list of tuples of the form (apiname, achieved, unlocktime, name, description)
@@ -43,14 +53,13 @@ def GetPlayerAchievements(steam_id, app_id):
         with urllib.request.urlopen(url) as response:
             data = response.read()
             if not data:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+                raise NoMatches("No matching data for input parameters")
             result = json.loads(data) 
             game_name = result["playerstats"]["gameName"]
             achievement_data = [(achievement["apiname"], achievement["achieved"], achievement["unlocktime"], achievement["name"], achievement["description"]) for achievement in result["playerstats"]["achievements"]]
             return game_name, achievement_data
     except Exception as e:
-        #Request failed
-        raise Exception(f"Error: {e}")
+        raise RequestFail(f"Error: {e}")
 
 #Retrieves info about users game library, returns number of games owned and a list of tuples of the form (app_id, total_playtime, app_img_url, last_time_played)
 def GetOwnedGames(steam_id):
@@ -59,14 +68,13 @@ def GetOwnedGames(steam_id):
         with urllib.request.urlopen(url) as response:
             data = response.read()
             if not data:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+                raise NoMatches("No matching data for input parameters")
             result = json.loads(data) 
             game_count = result["response"]["game_count"]
             games = [(game["appid"], game["playtime_forever"], "http://media.steampowered.com/steamcommunity/public/images/apps/"+str(game["appid"])+"/"+game["img_icon_url"]+".jpg", game["rtime_last_played"]) for game in result["response"]["games"]]
             return game_count, games
     except Exception as e:
-        #Request failed
-        raise Exception(f"Error: {e}")
+        raise RequestFail(f"Error: {e}")
 
 #Retrieves info about a players recent game data, returns number of games played in the last 2 week, and a list of tuples of the form (app_id, playtime_last_2_weeks)
 def GetRecentlyPlayedGames(steam_id):
@@ -75,14 +83,13 @@ def GetRecentlyPlayedGames(steam_id):
         with urllib.request.urlopen(url) as response:
             data = response.read()
             if not data:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+                raise NoMatches("No matching data for input parameters")
             result = json.loads(data) 
             game_count = result["response"]["total_count"]
             games = [(game["appid"], game["playtime_2weeks"]) for game in result["response"]["games"]]
             return game_count, games
     except Exception as e:
-        #Request failed
-        raise Exception(f"Error: {e}")
+        raise RequestFail(f"Error: {e}")
 
 
 #Retrieves possibly useful metadata about a given app, returns the names name, a list of developers, a list of publishers, a list of tuples of the form (genre_id, genre_name)
@@ -93,13 +100,11 @@ def GetAppMetadata(app_id):
         with urllib.request.urlopen(url) as response:
             data = response.read()
             if not data:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+                raise NoMatches("No matching data for input parameters")
             result = json.loads(data) 
-            if result[str(app_id)]["success"] != True:
-                raise Exception("Error: Invalid input parameters or the provided account is private")
+            if result[str(app_id)]["success"] != TrNoMatches
             metadata = result[str(app_id)]["data"]
             return  metadata["name"], metadata["developers"], metadata["publishers"], [(genre["id"], genre["description"]) for genre in metadata["genres"]], metadata["release_date"]["date"]
     except Exception as e:
-        #Request failed
-        raise Exception(f"Error: {e}")
+        raise RequestFail(f"Error: {e}")
       
