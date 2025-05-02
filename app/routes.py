dea7be1b95bc3ced9d970ce3c029b33801c5a1b3
Jacobpop123 <126186512+Jacobpop123@users.noreply.github.com>
2025-05-03 01:28:06 +0800

    Update routes.py
    major changes to routes to support login and register. Added checks such if user exists, if password is correct and if in you're in session to see if already logged in. Added checks like if username already exists and adding users to database.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, redirect, url_for, flash, session
+from app import app, db 
+from app.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.security import *
+
 
 @app.route('/')
 @app.route('/home')
@@ -18,13 +22,84 @@ def view():
 def share():
     return render_template('main/share.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'user_id' in session:
+        flash('You are already logged in.', 'info')
+        return redirect(url_for('home'))
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        #check if user exists
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            flash('Username does not exist.' , 'danger')
+            return redirect(url_for('login'))
+        
+        if not check_password(user.password_hash, password):
+            flash('Incorrect password.', 'danger')
+            return redirect(url_for('login'))
+        
+        #successful login
+        else:
+            session['user_id'] = user.id
+            session['username'] = user.username
+            flash('Login successful', 'success')
+            return redirect(url_for('home'))
+            
+        
+
     return render_template('main/login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if 'user_id' in session:
+        flash('You are already logged in.', 'info')
+        return redirect(url_for('home'))
+     
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        
+        #check if passwords match
+        if password != confirm_password:
+            flash('Passwords do not match.', 'danger')
+            return redirect(url_for('register'))
+        
+        #check if username already exist
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists.', 'danger')
+            return redirect(url_for('register'))
+        
+        #new user instance
+        new_user = User(username=username)
+        new_user.set_password(password)
+
+        #add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account creation successful', 'success')
+        return redirect(url_for('login'))
     return render_template('main/register.html')
+
+@app.route('/profile')
+def profile():
+    if 'user_id' not in session:
+        flash("Please log in to access your profile.", "warning")
+        return redirect(url_for('login'))
+
+    return render_template('main/profile.html', username=session['username'])
+
+
+@app.route('/logout', methods = ['POST'])
+def logout():
+    session.clear()
+    flash("You have been logged out.", "info")
+    return redirect(url_for('home'))
 
 @app.route('/generate')
 def generate():
