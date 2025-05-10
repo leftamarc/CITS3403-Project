5d49e473ca6b26b73e3a64ca52924661e6f979c4
Jacobpop123 <126186512+Jacobpop123@users.noreply.github.com>
2025-05-11 06:59:43 +0800

    Update __init__.py
    update to add flask limiter (brute force prevention)

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 import os
 import sys
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_login import LoginManager 
+
+
 
 #Checks for STEAM_API_KEY
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
@@ -23,12 +27,21 @@ login_manager.login_view = 'login'  # Set default login view
 login_manager.init_app(app)
 
 
-#generate secret key 
+# Generate secret key 
 app.config['SECRET_KEY'] = os.urandom(24)
-from app import routes, models
+
 
 # Define the user_loader function
 from app.models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))  
+
+# Initialise limiter
+limiter = Limiter(
+    key_func=get_remote_address,
+    app =app,
+    default_limits=["200 per day", "50 per hour"],
+)
+limiter.init_app(app)
+from app import routes, models
