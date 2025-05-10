8fdbd83180713a7a4f529e8e0ba9e8e20cebfdc9
Jacobpop123 <126186512+Jacobpop123@users.noreply.github.com>
2025-05-11 04:33:16 +0800

    Update security.py
    added check for strong password

--- app/security.py
+++ app/security.py
@@ -1,4 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
 
 #function to hash a password
 def hash_password(password):
@@ -7,3 +8,12 @@ def hash_password(password):
 #function to check if the given password matches the hashed password
 def check_password(hashed_password, password):
     return check_password_hash(hashed_password, password)
+
+#function for strong password
+def is_strong_password(password):
+    return (
+        len(password) >= 8 and
+        re.search(r'[A-Za-z]', password) and
+        re.search(r'\d', password) and
+        re.search(r'[!@#$%^&*(),.?":{}|<>]', password)
+    )
