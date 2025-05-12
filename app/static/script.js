bb4e5bd5c8469b1dded39e68a34b9098f9d9bab1
RS-23777804 <ryanschofield365@gmail.com>
2025-05-12 17:22:12 +0800

    Added a call to the positionAnimatedElements function inside of view_card to solve animated background issue.

--- app/static/script.js
+++ app/static/script.js
@@ -6,8 +6,11 @@ document.querySelector('form').addEventListener('submit', function(event) {
         alert('Passwords do not match!');
     }
 });
-
 document.addEventListener("DOMContentLoaded", () => {
+    positionAnimatedElements();
+});
+
+function positionAnimatedElements() {
     const elements = document.querySelectorAll("ul#elements li");
     const elementPositions = []; 
 
@@ -25,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
             isOverlapping = elementPositions.some(pos => {
                 const distanceX = Math.abs(pos.x - randomX);
                 const distanceY = Math.abs(pos.y - randomY);
-                return distanceX < 0.1 && distanceY < 0.1; // Adjust overlap threshold as needed
+                return distanceX < 0.1 && distanceY < 0.1;
             });
         } while (isOverlapping);
 
@@ -37,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
         element.style.setProperty('--randomY', randomY);
         element.style.setProperty('--randomSize', randomSize);
     });
-});
+};
 
 document.addEventListener("DOMContentLoaded", () => {
     let formSubmitted = false;
