dc11c3622c4a1e6144d4a7238b3be24322048572
OldNan <jacobpop123@gmail.com>
2025-05-16 02:46:28 +0800

    login.html: added WTforms register.html: added WTforms security.py added login_needed function (avoid conflicts with flask session and flask login), implemented WTforms routes.py edited to allow WTforms, and got rid of flask login (added own login_needed wrapped function) to avoid conflicts between flask session and flask login (reason for remember me errors) __init.py__ changed secret key to a static secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 import os
 import sys
-from flask import Flask
+from flask import Flask, session
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-from flask_login import LoginManager
+ 
+
 
 # Initialise extensions
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
-limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
+limiter = Limiter(key_func=get_remote_address, default_limits=["600 per day", "300 per hour"])
 
 # Checks for STEAM_API_KEY
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
@@ -20,27 +20,27 @@ if not STEAM_API_KEY:
     print("Error: STEAM_API_KEY environment variable is not set.")
     sys.exit(1)
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # set secret key
+    app.config['SECRET_KEY'] = b'\x07\xce]\x8fC\xe0\x17\xae6\xfd\xff\x1b\xe5yZd\x88z`\x88\t+O\x0b'
+
     db.init_app(app)
     migrate.init_app(app, db)
-    login_manager.init_app(app)
     limiter.init_app(app)
-
-    from app.blueprint import blueprint
-    app.register_blueprint(blueprint)
-
-    login_manager.login_view = '/login'
-
-    # Generate secret key 
-    app.config['SECRET_KEY'] = os.urandom(24)
 
     # Define the user_loader function
     from app.models import User
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+    
+    from app.blueprint import blueprint
+    app.register_blueprint(blueprint)
+    
 
     return app
+
+
+
+
