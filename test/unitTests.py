e214f5eb608c64836cd8926c96ab6177c4bb272f
RS-23777804 <ryanschofield365@gmail.com>
2025-05-15 20:01:52 +0800

    Implemented Testing with unittest module 1/5+ tests complete

--- test/unitTests.py
+++ test/unitTests.py
@@ -1,13 +1,30 @@
 import unittest
 
-from app import create_app
-from app.config import TestingConfig 
+from app import create_app, db
+from app.config import TestingConfig
+from app.models import User 
 
 
-class UnitTests(unittest.TestCase):
-    def Setup(self):
+class BasicTests(unittest.TestCase):
+    def setUp(self):
         testApp = create_app(TestingConfig)
+        self.app_context = testApp.app_context()
+        self.app_context.push()
+        db.create_all()
+
         return super().setUp()
     
+    def test_user_creation(self):
+        user = User(username='testuser')
+        password = 'testpassword1!'
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+
+        self.assertIsNotNone(user)
+
     def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
         return super().tearDown()
