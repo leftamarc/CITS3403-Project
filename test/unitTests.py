9a80cf0fc81c9e4776e15fb550f7bf94393927ae
RS-23777804 <ryanschofield365@gmail.com>
2025-05-15 20:47:53 +0800

    Implemented 5 Unit tests related to user details

--- test/unitTests.py
+++ test/unitTests.py
@@ -2,7 +2,8 @@ import unittest
 
 from app import create_app, db
 from app.config import TestingConfig
-from app.models import User 
+from app import models 
+from app import security
 
 
 class BasicTests(unittest.TestCase):
@@ -14,14 +15,48 @@ class BasicTests(unittest.TestCase):
 
         return super().setUp()
     
-    def test_user_creation(self):
-        user = User(username='testuser')
-        password = 'testpassword1!'
+    def create_user(self, username, password):
+        user = models.User(username=username)
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
+        return user
 
+    def test_user_creation(self):
+        user = self.create_user('testuser', 'testpassword1!')
         self.assertIsNotNone(user)
+
+    def test_user_login(self):
+        self.create_user('testuser2', 'testpassword2!')
+        password = 'testpassword2!'
+
+        logged_in_user = models.User.query.filter_by(username='testuser2').first()
+        self.assertIsNotNone(logged_in_user)
+        self.assertFalse(logged_in_user.check_password(password + 'willywonka'))
+        self.assertTrue(logged_in_user.check_password(password))
+
+    def test_password_hashing(self):
+        user = models.User(username='anotheruser')
+        user.set_password('securepassword1!')
+        self.assertTrue(user.check_password('securepassword1!'))
+        self.assertFalse(user.check_password('wrongpassword1!'))
+
+    def test_user_query(self):
+        user = models.User(username='queryuser')
+        user.set_password('password1!')
+        db.session.add(user)
+        db.session.commit()
+        queried = models.User.query.filter_by(username='queryuser').first()
+        self.assertIsNotNone(queried)
+        self.assertEqual(queried.username, 'queryuser')
+
+    def test_password_strength(self):
+        self.assertTrue(security.is_strong_password('StrongPass1!'))
+        self.assertFalse(security.is_strong_password('weakpass'))
+        self.assertFalse(security.is_strong_password('12345678'))
+        self.assertFalse(security.is_strong_password('NoSpecialChar1'))
+        self.assertFalse(security.is_strong_password('!@#$%^&*()'))
+
 
     def tearDown(self):
         db.session.remove()
