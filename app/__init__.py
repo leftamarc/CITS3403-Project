fc873ed429a9b0dfc5038a521ca4916e88eb1132
RS-23777804 <ryanschofield365@gmail.com>
2025-05-15 15:09:17 +0800

    Implemented blueprint and application factory required for testing and prod.

--- app/__init__.py
+++ app/__init__.py
@@ -6,42 +6,41 @@ from flask_limiter.util import get_remote_address
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-from flask_login import LoginManager 
+from flask_login import LoginManager
 
+# Initialise extensions
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
 
-
-#Checks for STEAM_API_KEY
+# Checks for STEAM_API_KEY
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
 if not STEAM_API_KEY:
     print("Error: STEAM_API_KEY environment variable is not set.")
     sys.exit(1)
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-# Initialise flask-login
-login_manager = LoginManager()
-login_manager.login_view = '/login'  # Set default login view
-login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    limiter.init_app(app)
 
+    from app.blueprint import blueprint
+    app.register_blueprint(blueprint)
 
-# Generate secret key 
-app.config['SECRET_KEY'] = os.urandom(24)
+    login_manager.login_view = '/login'
 
+    # Generate secret key 
+    app.config['SECRET_KEY'] = os.urandom(24)
 
-# Define the user_loader function
-from app.models import User
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))  
+    # Define the user_loader function
+    from app.models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-# Initialise limiter
-limiter = Limiter(
-    key_func=get_remote_address,
-    app =app,
-    default_limits=["200 per day", "50 per hour"],
-)
-limiter.init_app(app)
-from app import routes, models
+    return app
--- app/project.py
+++ app/project.py
@@ -1,1 +1,8 @@
-from app import app
+from flask_migrate import Migrate
+from app import create_app, db
+from app.config import DeploymentConfig
+
+if __name__ == '__main__':
+    app = create_app(DeploymentConfig)
+    migrate = Migrate(app, db)
+    app.run(debug=True, use_debugger=False, use_reloader=False)
